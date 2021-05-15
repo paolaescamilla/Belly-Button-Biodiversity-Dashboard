59d36efeb9b2eef81e1bb11dad275aa7bc83f0fc
paolaescamilla <78698456+paolaescamilla@users.noreply.github.com>
2021-05-15 14:36:41 -0400

    Final code for challenge

--- js/charts.js
+++ js/charts.js
@@ -73,7 +73,7 @@ var result = filteredsamplesArray [0];
 // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
 var otu_ids = result.otu_ids;
 var otu_labels = result.otu_labels;
-var sample_values = result.sample_values;
+var sample_values = result.sample_values; 
 
 // 7. Create the yticks for the bar chart.
 // Hint: Get the the top 10 otu_ids and map them in descending order  
